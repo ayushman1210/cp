86dc644d18667acd972767b1b5d2151615c1cfa4
ayushman1210 <ayushmangupta892@gmail.com>
2025-03-01 23:09:18 +0530

    change

--- luckyno.cpp
+++ luckyno.cpp
@@ -3,19 +3,10 @@ using namespace std;
 int main(){
 int t;
 cin>>t;
-int ld=0;
-int count=0;
-while(t>0){
-    ld=t%10;
-if(ld==7 || ld==4){
-    count++;
-}
-t/=10;
-}
-if(count==7 || count==4){
-    cout<<"YES";
-}
-else{
-    cout<<"NO";
+int a,b,c;
+while(t--){
+cin>>a>>b>>c;
+if((a+b)==c) cout<<"+"<<endl;
+else if((a-b)==c)cout<<"-"<<endl;
 }
 }
--- translation.cpp
+++ translation.cpp
@@ -1,8 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-string s;
-getline(cin,s);
-string t;
-
+int a,b,c,d;
+int max=a;
+cin>>a>>b>>c>>d;
+if(a>b && a>c && a>d){
+    max=a;
+}
+if(b>a && b>c && b>d){
+    max=b;
+}
+if(c>b && c>a && c>d){
+    max=c;
+}
+if(d>b && d>c && d>a){
+    max=d;
+}
+if(max==a){
+    cout<<max-b<<" "<<max-c<<" "<<max-d;
+}
+if(max==b){
+    cout<<max-a<<" "<<max-c<<" "<<max-d;
+}
+if(max==c){
+    cout<<max-b<<" "<<max-a<<" "<<max-d;
+}
+if(max==d){
+    cout<<max-b<<" "<<max-c<<" "<<max-a;
+}
 }
